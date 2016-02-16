dc9f43d365ccf59d518b9d1bb6bdd09e24d3b194
Doron Tal <doron@tracktunes.org>
2016-02-16 03:31:14 -0500

    now hiding tabs section via app.scss.  still got android chrome bug

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -16,11 +16,4 @@ export class TabsPage {
         this.tab1Root = RecordPage;
         this.tab2Root = LibraryPage;
     }
-
-    onPageDidEnter() {
-        this.app.setTitle('Ionic Record');
-        // globally hide the tab-bar - only use tabs programmatically, see:
-        //     https://forum.ionicframework.com/t/ionic2-hide-tabs/37998/4
-        document.querySelector('#tabs ion-tabbar-section').style.display = 'none';
-    }
 }
